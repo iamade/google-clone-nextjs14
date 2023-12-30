8b274506299b7fd1fcf28995c9b8ee8c4f506c80
Adesegun Koiki <kamar3deen@gmail.com>
2023-12-30 21:17:05 +0100

    install next js first template

---
+++ src/app/head.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+    <>
+       <title>Google Clone Next Js 14</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <meta name="description" content="Google clone created by Next Js 13" />
+        <link rel="icon" href="/favicon.ico"/>
+    </>
+   
+  )
+}
